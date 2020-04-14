4c32d96396418a6e39df2228bb71da018c54ed45
Raakost <jeanettesydboege@gmail.com>
2020-04-14 13:48:01 +0200

    More homepage

--- Shop/Views/Home.php
+++ Shop/Views/Home.php
@@ -1,14 +1,19 @@
 <?php ?>
 <!DOCTYPE html>
-<html>
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../assets/css/w3shopcss.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Helvetica Neue">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<html lang="en">
+<head>
+    <title>Duck Shop</title>
+    <meta charset="UTF-8">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../assets/css/w3shopcss.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Helvetica Neue">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
 <style>
     .w3-sidebar a {
         font-family: "Montserrat", "Helvetica Neue", Arial, sans-serif;
@@ -17,11 +22,15 @@
     h1, h2, h3, h4, h5, h6, .w3-wide {
         font-family: "Montserrat", "Helvetica Neue", Arial, sans-serif;
     }
+    body {
+        background-color: #f4f3ef;
+    }
+
 </style>
 <body class="w3-content" style="max-width:1200px">
 
 <!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px;" id="mySidebar">
+<nav class="w3-sidebar w3-bar-block w3-light w3-collapse w3-top" style="z-index:3;width:250px;" id="mySidebar">
     <div class="w3-container w3-display-container w3-padding-16">
         <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
         <img style="width: 80px;" src="../assets/img/logo.png" class="center-image" alt="">
@@ -29,7 +38,7 @@
     </div>
     <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
         <a href="#" class="w3-bar-item w3-button">Duck Shop</a>
-        <a href="#" class="w3-bar-item w3-button">News</a>
+        <a href="#news-section" class="w3-bar-item w3-button">News</a>
         <a href="#" class="w3-bar-item w3-button">Daily Special</a>
     </div>
     <div class="w3-large w3-text-grey" style="font-weight:bold">
@@ -62,22 +71,21 @@
     <!-- Top header -->
     <header class="w3-container w3-xlarge">
         <p class="w3-right">
-            
         </p>
     </header>
 
-    <!-- Image header -->
+    <!-- Daily Special -->
     <div class="w3-display-container w3-container">
         <img src="../assets/img/ducksonrow.jpeg" style="width:100%; opacity: 80%;">
         <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
             <h1 class="w3-hide-small">DAILY SPECIAL OFFER</h1>
             <h1 class="w3-hide-large w3-hide-medium">DAILY SPECIAL OFFER</h1>
             <h1 class="w3-large w3-hide-small">Check it out... </h1>
-            <p><a href="" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
+            <p><a href="#" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
         </div>
     </div>
-    <div class="w3-container w3-text-grey" id="news-section">
-        <h2>News</h2>
+    <div class="w3-container w3-text-grey w3-margin-top-big" id="news-section">
+        <h2>LATEST NEWS</h2>
     </div>
 
     <!-- Product grid -->
@@ -118,18 +126,15 @@
     </div>
 
     <!-- Subscribe section -->
-    <div class="w3-container w3-black w3-padding-32">
-        <h1>Subscribe</h1>
-        <p>To get special offers and VIP treatment:</p>
-        <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:100%"></p>
-        <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
+    <div class="w3-container w3-black w3-padding-64 w3-margin-top-big">
+
     </div>
 
     <!-- Footer -->
-    <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
+    <footer class="w3-padding-64 footer-background w3-small w3-center" id="footer">
         <div class="w3-row-padding">
             <div class="w3-col s4">
-                <h4>Contact</h4>
+                <h4>CONTACT</h4>
                 <p>Questions? Go ahead.</p>
                 <form action="/action_page.php" target="_blank">
                     <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
@@ -139,58 +144,39 @@
                     <button type="submit" class="w3-button w3-block w3-black">Send</button>
                 </form>
             </div>
-
-
             <div class="w3-col s4">
-                <h4>Store</h4>
-                <p><i class="fa fa-fw fa-map-marker"></i> Company Name</p>
-                <p><i class="fa fa-fw fa-map-marker"></i> streetname no</p>
-                <p><i class="fa fa-fw fa-map-marker"></i> zip/city</p>
-                <p><i class="fa fa-fw fa-map-marker"></i> company</p>
+                <h4>STORE</h4>
+                <p></i> Company Name</p>
+                <p><i class="fa fa-fw fa-map-marker"></i> streetname/number</p>
+                <p> zip/city</p>
+                <p> country</p>
                 <p><i class="fa fa-fw fa-phone"></i> phone</p>
                 <p><i class="fa fa-fw fa-envelope"></i> ex@mail.com</p>
             </div>
-            <div class="w3-col s4 w3-justify">
-                <h4>About</h4>
-                <p>Company info description here</p>
+            <div class="w3-col s4">
+                <h4>ABOUT</h4>
+                <p class="w3-justify">Company info description here Company info description here</p>
             </div>
         </div>
     </footer>
 
-    <div class="w3-black w3-center w3-padding-24">
-        <h1>opening hours here</h1>
+    <div class="w3-black w3-center w3-padding-32 w3-small">
+        <h5 class="w3-padding-32">OPENING HOURS</h5>
+        <ul class="w3-tiny">
+            <li class="display-li-inline">Mon 08:00 - 17:00 </li>
+            <li class="display-li-inline">Tue 08:00 - 17:00 </li>
+            <li class="display-li-inline">Wed 08:00 - 17:00 </li>
+            <li class="display-li-inline">Thur 08:00 - 17:00 </li>
+            <li class="display-li-inline">Fri 08:00 - 17:00 </li>
+            <li class="display-li-inline">Sat 08:00 - 17:00 </li>
+            <li class="display-li-inline">Sun 08:00 - 17:00 </li>
+        </ul>
     </div>
 
     <!-- End page content -->
 </div>
 
-<!-- Newsletter Modal -->
-<div id="newsletter" class="w3-modal">
-    <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
-        <div class="w3-container w3-white w3-center">
-            <i onclick="document.getElementById('newsletter').style.display='none'"
-               class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
-            <h2 class="w3-wide">NEWSLETTER</h2>
-            <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
-            <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
-            <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom"
-                    onclick="document.getElementById('newsletter').style.display='none'">Subscribe
-            </button>
-        </div>
-    </div>
-</div>
-
 <script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-
     // Click on the "Jeans" link on page load to open the accordion for demo purposes
     document.getElementById("myBtn").click();
 
