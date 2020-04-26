77a6e10b2aac75cde44bb0b793dbc8254846a25d
Raakost <jeanettesydboege@gmail.com>
2020-04-26 23:50:15 +0200

    Update Constants.php

--- DBConnection/Constants.php
+++ DBConnection/Constants.php
@@ -1,1 +1,19 @@
+<?php
 
+//$db_name = "mysql:dbname=wtd;";
+//$db_host = "host=localhost;";
+//$db_charset = "charset=utf8";
+
+//define("DSN", "$db_name $db_host $db_charset");
+//define("DB_USER", "jean");
+//define("DB_PASS", "123456");
+
+
+$db_name = "mysql:dbname=u976192968_WTD;";
+$db_host = "host=localhost;";
+$db_charset = "charset=utf8";
+
+
+define("DSN", "$db_name $db_host $db_charset");
+define("DB_USER", "u976192968_JeanAdmin");
+define("DB_PASS", "17eretprimtal!");
